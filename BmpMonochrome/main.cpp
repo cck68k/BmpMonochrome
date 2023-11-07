8c499620d9b0a2bdfbdc0bd2879c0acbbb9aa6fe
cck68k <00mewoawaseru00@gmail.com>
2023-11-07 22:57:26 +0900

    We can now set Filename of i/o

--- BmpMonochrome/main.cpp
+++ BmpMonochrome/main.cpp
@@ -1,10 +1,14 @@
 #include<vector>
 #include<iostream>
+#include<string>
 #include<Windows.h>
 #include"BitmapIOSample.h"
 
 int main()
 {
+    std::string inputImageName;
+    std::string outputImageName;
+
     unsigned int averageColor1 = 0x0;
     unsigned int averageColor2 = 0x0;
     unsigned int averageColor3 = 0x0;
@@ -17,6 +21,8 @@ int main()
     unsigned int resultColor5 = 0x0;
     unsigned int resultColor6 = 0x0;
 
+    std::cin >> inputImageName;
+    std::cin >> outputImageName;
     std::cin >> averageColor1;
     std::cin >> averageColor2;
     std::cin >> averageColor3;
@@ -34,9 +40,9 @@ int main()
     BitmapManager bitmapManager;
     std::vector<byte> pixels;
 
-    bitmapManager.LoadBitmap24("./bmp/NoelleGuts.bmp", pixels);
+    bitmapManager.LoadBitmap24(inputImageName, pixels);
     bitmapManager.EditBitmap(pixels, averageColor1, averageColor2, averageColor3, averageColor4, averageColor5, resultColor1, resultColor2, resultColor3, resultColor4, resultColor5, resultColor6);
-    bitmapManager.SaveBitmap24("./bmp/NoelleGutsoutput.bmp", pixels.data());
+    bitmapManager.SaveBitmap24(outputImageName, pixels.data());
 
     return 0;
 }
@@ -78,4 +84,18 @@ unsigned char averageColor1 = 0x0;
 0
 0
     にしたらaverageColor1に"2"、averageColor2に"3"、averageColor3に"0"。勝手に文字型にするのか…
+
+./bmp/NoelleGuts.bmp
+./bmp/NoelleGutsoutput.bmp
+230
+210
+170
+90
+50
+255
+150
+50
+0
+0
+0
 */
